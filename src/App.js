32039f2bd735cfd11bec0b5640db4be19bb3a022
Musm1 <musm123456@gmail.com>
2023-08-20 12:07:11 +0500

    test req
    test req

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopBar from './compoents/TopBar';
+import Inputs from './compoents/Inputs';
+import TimeLocation from './compoents/TimeLocation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br
+    from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
+      <TopBar/>
+      <Inputs/>
+
+      <TimeLocation/>
     </div>
   );
 }
---
+++ src/compoents/TimeLocation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function TimeLocation() {
+  return (
+    <div className=''>
+        <div className='flex items-center justify-center my-6'>
+            <p className='text-white text-xl font-extralight'>
+                Tuesday, 31 May, 2022 | 12.36pm
+            </p>
+        </div>
+        <div className='flex items-center justify-center my-3'>
+            <p className='text-white text-3xl font-medium'>
+                Karachi, PK
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default TimeLocation
